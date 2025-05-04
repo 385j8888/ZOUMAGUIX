da3f029cd37599c81119e2294060fc59c7547119
385j8888 <2586125757@qq.com>
2025-05-05 03:22:08 +0800

    Add files via upload

--- 走马观花X生存7天.lua
+++ 走马观花X生存7天.lua
@@ -99,11 +99,11 @@ gn:Toggle("枪械杀戮光环(鹿)", "", false, function(state)
         -- 动态替换坐标，保留旋转
                               local args = {
                                   [1] = CFrame.new(
-                                      deerPosition.X, deerPosition.Y, deerPosition.Z-- 替换为当前deer的位置
+                                      deerPosition.X, deerPosition.Y, deerPosition.Z,  deerPosition.X, deerPosition.Y, deerPosition.Z, deerPosition.X, deerPosition.Y, deerPosition.Z, deerPosition.X, deerPosition.Y, deerPosition.Z
                 --unpack(originalRotation1) -- 保留原有旋转参数
                                   ),
                                   [2] = CFrame.new(
-                                      deerPosition.X, deerPosition.Y, deerPosition.Z -- 替换为当前deer的位置
+                                      deerPosition.X, deerPosition.Y, deerPosition.Z,  deerPosition.X, deerPosition.Y, deerPosition.Z, deerPosition.X, deerPosition.Y, deerPosition.Z, deerPosition.X, deerPosition.Y, deerPosition.Z-- 替换为当前deer的位置
                 --unpack(originalRotation2) -- 保留原有旋转参数
                                   )
                               }
@@ -141,10 +141,10 @@ gn:Toggle("枪械杀戮光环(怪物)", "", false, function(state)
         -- 构建动态 CFrame（替换位置，保留旋转）
                           local args = {
                               [1] = CFrame.new(
-                                  scpPosition.X, scpPosition.Y, scpPosition.Z
+                                  scpPosition.X, scpPosition.Y, scpPosition.Z,  deerPosition.X, deerPosition.Y, deerPosition.Z, deerPosition.X, deerPosition.Y, deerPosition.Z, deerPosition.X, deerPosition.Y, deerPosition.Z
                               ),
                               [2] = CFrame.new(
-                                  scpPosition.X, scpPosition.Y, scpPosition.Z
+                                  scpPosition.X, scpPosition.Y, scpPosition.Z,  deerPosition.X, deerPosition.Y, deerPosition.Z, deerPosition.X, deerPosition.Y, deerPosition.Z, deerPosition.X, deerPosition.Y, deerPosition.Z
                               )
                           }
 
@@ -198,7 +198,7 @@ for _, deer in pairs(workspace.interact:GetChildren()) do
 end
 end)
 local autocllogggg = false
-gn:Toggle("快速挥斧头(秒砍树)", "", false, function(state)
+gn:Toggle("自动砍树", "", false, function(state)
     autocllogggg = state  -- 同步阀门状态
     
     if state then
@@ -341,4 +341,8 @@ end)
 playerr:Button("飞行",function()
   loadstring(
         "\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+end)
+playerr:Button("解锁视角(第三人称)",function()
+game:GetService("Players").LocalPlayer.CameraMaxZoomDistance = 99999
+game:GetService("Players").LocalPlayer.CameraMode = Enum.CameraMode.Classic
 end)
