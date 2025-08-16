1c3eb9c18a3ff7b3602bc2f0b2a9c397ded88442
385j8888 <2586125757@qq.com>
2025-08-16 13:08:11 +0800

    Add files via upload

--- 走马脑叶.lua
+++ 走马脑叶.lua
@@ -71,7 +71,7 @@ gn:Textbox("攻速", "攻速", "请输入攻速", function(value)
     end
 end)
 local tsyxt = false
-gn:Toggle("透视所有生物", "", false, function(state)
+gn:Toggle("透视所有异想体", "", false, function(state)
     tsyxt = state  -- 同步阀门状态
     
     if state then
@@ -118,6 +118,7 @@ gn:Toggle("透视所有生物", "", false, function(state)
                          end
                      end
                  end
+                 wait(5)
            end
       --   end)
     else
