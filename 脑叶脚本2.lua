5134d28a80e973d52c930ae75c30514a01bfb7f3
385j8888 <2586125757@qq.com>
2025-08-22 17:55:10 +0800

    Add files via upload

--- 脑叶脚本2.lua
+++ 脑叶脚本2.lua
@@ -95,6 +95,8 @@ elseif a=="wuzhikai123"then
 _G.white=true
 elseif a=="zhangjinpeng7951www"then
 _G.white=true
+elseif a=="VEARwarwar"then
+_G.white=true
 end
 if _G.white==true then
     print("验证成功")
